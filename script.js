403d675ec79f45f1fd2bde173479df567269fb41
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-06-27 02:03:37 +0100

    section2

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 hamburger.addEventListener('click', () => menuLinks.classList.toggle('activeShow'))
 
 changeicon = (bars) => bars.classList.toggle("fa-times"); 
+
+const people = document.querySelector('.people');
+let url = 'https://fakestoreapi.com/products';
+let peopleData = [];
