41741f1425362be24b275e8c2f671d5ae3a45cb5
TeNeBiT-eng <ademolababalola2003@gmail.com>
2022-07-05 15:11:57 +0100

    ...

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 const furnitureContainer = document.querySelector('.funitureItems');
-const url = 'https://fakestoreapi.com/products?limit=1'
+const url = 'https://fakestoreapi.com/products?limit=10'
 
 fetch(url)
 .then((resp) => resp.json())
--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function myFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+      let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
